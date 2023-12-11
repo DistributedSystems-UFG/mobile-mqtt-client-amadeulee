2dceb86fc73d2a7dc0e52b79267dfd958a54299f
Amadeu Lee <amadeu.lee@townsq.com.br>
2023-12-10 23:54:06 -0300

    alteracao da aplicacao, criacao do PDF com explicacao da aplicacao

--- app/src/main/java/com/example/basicandroidmqttclient/DisplayMessageActivity.java
+++ app/src/main/java/com/example/basicandroidmqttclient/DisplayMessageActivity.java
@@ -2,7 +2,9 @@ package com.example.basicandroidmqttclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+    }
+
+    public void goToMainActivity(View view) {
+        // Implement logic to go back to MainActivity
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
